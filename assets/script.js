fb4eaf81942cdf3dfbb5b2a92304fa702e104fae
Tuanemendes <tuane.belmiro@gmail.com>
2023-07-17 00:22:02 -0300

    implementação do projetos

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,9 @@ function clickMenu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('active');
 }
+
+window.addEventListener('scroll', function () {
+    var header = document.querySelector('[data-header]');
+    header.classList.toggle('scrolling', window.scrollY > 0);
+  
+  });
